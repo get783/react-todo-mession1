7cff5fb6440bba76eebcd041e7a143234ebf53fb
leejiyeon <sky75013@naver.com>
2025-07-09 22:43:42 +0900

    feat: 할 일 추가, 삭제, 체크, 전역 관리

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoWriteForm from './components/TodoWriteForm'
+import TodoList from './components/TodoList'
+
+function App() {
+    return (
+        <>
+            <TodoWriteForm />
+            <TodoList />
+        </>
+    )
+}
+
+export default App
